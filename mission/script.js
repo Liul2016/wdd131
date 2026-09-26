c214fc05ae2533b4a2d0b1fee2a2f7b19a7e91df
Liul2016 <ryan.c.lee1018@gmail.com>
2026-09-26 15:45:29 -0600

    W02 Prove: Mission Statement II

---
+++ mission/script.js
@@ -0,0 +1,15 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.classList.add('dark-mode');
+        logo.setAttribute('src', 'images/byui-logo-white.png');
+    } else {
+        document.body.classList.remove('dark-mode');
+        logo.setAttribute('src', 'images/byui-logo-blue.webp');
+    }
+}
